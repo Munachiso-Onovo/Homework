be5152d2b0e926bbf578629295866ade8101ce63
Munachiso-Onovo <munachisoonovo@gmail.com>
2024-11-30 14:05:55 +0000

    Add files via upload

---
+++ first.py
@@ -0,0 +1,13 @@
+import pgzrun
+
+def draw():
+    screen.clear()
+    screen.draw.text("welcome to pgzero",(300,300),color = "red")
+    screen.draw.text("welcome to pgzero",(300,0),color = "red")
+    screen.draw.text("welcome to pgzero",(0,300),color = "red")
+    screen.draw.text("welcome to pgzero",(600,300),color = "red")
+    screen.draw.text("welcome to pgzero",(300,600),color = "red")
+    screen.draw.text("welcome to pgzero",(600,300),color = "red")
+
+pgzrun.go()
+
